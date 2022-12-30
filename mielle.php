cf759107851872d1fb2ee37602e064779a8f4e12
Rebecca <119697291+rbrantley0414@users.noreply.github.com>
2022-12-29 22:06:31 -0600

    mielle product page

---
+++ mielle.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mielle Organics | Creative Beauty Supply</title>
+    <meta aria-roledescription="About Us Creative Beauty Supply Store">
+    <link rel="icon" type="image/x-icon" href="imgs/creativebeautylogo.png" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="product.css">
+</head>
+
+<body>
+    <?php
+    include 'nav.php'; ?>
+    <div id="content">
+        <div id="vertical_nav">
+            <p>Shop By Brand</p>
+            <ul>
+                <li><a href="#">All</a></li>
+                <li>African Pride</li>
+                <li>Alikay Naturals</li>
+                <li>Aunt Jackie's</li>
+                <li>Camille Rose</li>
+                <li>Cantu</li>
+                <li>Got2B</li>
+                <li><a href="mielle.html">Mielle Organics</a></li>
+                <li>ORS</li>
+                <li>Shea Moisture</li>
+                <li>Shine N Jam</li>
+            </ul>
+        </div>
+        <div id="maincontent" class="container">
+            <h1>Mielle Organics</h1>
+            <div class="row">
+                <div class="col-lg-4 col-sm-6">
+                    <div class="thumbnail">
+                        <a href="#"><img src="../media/products/mielle/CBS-001489.jpg"></a>
+                        <div>
+                            <p>Mielle Rosemary Mint Scalp & Hair Strengthening Oil</p>
+                            <p>$9.99</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <div class="thumbnail">
+                        <img src="../media/products/mielle/CBS-001491.jpg">
+                        <div>
+                            <p>Mielle Rosemary Mint Strengthening Shampoo</p>
+                            <p>$9.99</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <div class="thumbnail">
+                        <img src="../media/products/mielle/CBS-001492.jpg">
+                        <div>
+                            <p>Mielle Rosemary Mint Daily Styling Creme</p>
+                            <p>$9.99</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <div class="thumbnail">
+                        <img src="../media/products/mielle/CBS-001493.jpg">
+                        <div>
+                            <p>Mielle Rosemary Mint Strengthening Edge Gel</p>
+                            <p>$6.99</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <div class="thumbnail">
+                        <img src="../media/products/mielle/CBS-002079.jpg">
+                        <div>
+                            <p>Mielle Moisture Rx Overnight Conditioner</p>
+                            <p>$9.99</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <div class="thumbnail">
+                        <img src="../media/products/mielle/CBS-002081.jpg">
+                        <div>
+                            <p>Mielle Moisture Rx Styling Gel</p>
+                            <p>$6.99</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <div class="thumbnail">
+                        <img src="../media/products/mielle/CBS-002082.jpg">
+                        <div>
+                            <p>Mielle Moisture Rx Scalp Treatment</p>
+                            <p>$9.99</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <div class="thumbnail">
+                        <img src="../media/products/mielle/CBS-762000.jpg">
+                        <div>
+                            <p>Mielle Detangling Co-Wash Pure Simple Natural</p>
+                            <p>$11.99</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <div class="thumbnail">
+                        <img src="../media/products/mielle/CBS-762001.jpg">
+                        <div>
+                            <p>Mielle White Peony Leave In Conditioner</p>
+                            <p>$12.99</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <div class="thumbnail">
+                        <img src="../media/products/mielle/CBS-762003.jpg">
+                        <div>
+                            <p>Mielle Babassu Oil & Mint Deep Conditioner</p>
+                            <p>$13.99</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <div class="thumbnail">
+                        <img src="../media/products/mielle/CBS-762006-1.jpg">
+                        <div>
+                            <p>Mielle Honey & Ginger Edge Gel</p>
+                            <p>$12.99</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <div class="thumbnail">
+                        <img src="../media/products/mielle/CBS-762038.jpg">
+                        <div>
+                            <p>Mielle Pomegranate & Honey Shampoo</p>
+                            <p>$12.99</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="page_nav">
+        <a href="#">
+            <div class="page_nav_square"><img src="./imgs/arrow-left-solid.svg" alt="arrow left page back">
+            </div>
+        </a>
+        <a href="#">
+            <div class="page_nav_square active_page">1</div>
+        </a>
+        <a href="mielle2.php">
+            <div class="page_nav_square">2</div>
+        </a>
+        <a href="mielle3.php">
+            <div class="page_nav_square">3</div>
+        </a>
+        <a href="mielle2.php">
+            <div class="page_nav_square"><img src="./imgs/arrow-right-solid.svg" alt="arrow right page forward">
+            </div>
+        </a>
+    </div>
+    <?php
+    include 'footer.php'; ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+
+</html>
